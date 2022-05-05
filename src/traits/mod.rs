acced9714c456308024e52afe8f7d6a6328587d3
Cem Karan <cfkaran2@gmail.com>
2022-05-05 11:41:14 -0400

    style: 'cargo +nightly fmt --all'
    Sorted all my use declarations into a sane sorting.

--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -8,32 +8,24 @@ pub mod async_fs_trait;
 pub mod async_read_dir_trait;
 pub mod async_sym_link_trait;
 
-#[doc(inline)]
-pub use async_dir_builder_trait::AsyncDirBuilderTrait;
-
-#[doc(inline)]
-pub use async_dir_entry_trait::AsyncDirEntryTrait;
-
-#[doc(inline)]
-pub use async_file_builder_trait::AsyncFileBuilderTrait;
-
-#[doc(inline)]
-pub use async_file_trait::AsyncFileTrait;
-
-#[doc(inline)]
-pub use async_fs_trait::AsyncFsTrait;
-
-#[doc(inline)]
-pub use async_read_dir_trait::AsyncReadDirTrait;
-
-#[doc(inline)]
-pub use async_sym_link_trait::AsyncSymLinkTrait;
-
-#[doc(no_inline)]
-pub use futures_io::{AsyncRead, AsyncBufRead, AsyncWrite, AsyncSeek};
-
 #[doc(no_inline)]
 pub use std::io::{Error, ErrorKind, IoSlice, IoSliceMut, Result, SeekFrom};
 
+#[doc(inline)]
+pub use async_dir_builder_trait::AsyncDirBuilderTrait;
+#[doc(inline)]
+pub use async_dir_entry_trait::AsyncDirEntryTrait;
+#[doc(inline)]
+pub use async_file_builder_trait::AsyncFileBuilderTrait;
+#[doc(inline)]
+pub use async_file_trait::AsyncFileTrait;
+#[doc(inline)]
+pub use async_fs_trait::AsyncFsTrait;
+#[doc(inline)]
+pub use async_read_dir_trait::AsyncReadDirTrait;
+#[doc(inline)]
+pub use async_sym_link_trait::AsyncSymLinkTrait;
 #[doc(no_inline)]
 pub use futures_core::stream::Stream;
+#[doc(no_inline)]
+pub use futures_io::{AsyncBufRead, AsyncRead, AsyncSeek, AsyncWrite};
